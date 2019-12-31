b1e95eb8d33201c021f82bb477ef897ff1a089e8
FionnMurphy <44289192+FionnMurphy@users.noreply.github.com>
2019-12-31 18:38:08 +0000

    merging websites

---
+++ assets/scripts/video_Hover.js
@@ -0,0 +1,8 @@
+var video = document.getElementById("vid");
+video.addEventListener("mouseover", function(){
+    video.play();
+})
+var video1 = document.getElementById("vid1");
+video1.addEventListener("mouseover", function(){
+    video1.play();
+})
