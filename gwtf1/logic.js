ed25a5451e62b9d18f948a8f1a65094a9ca73031
FionnMurphy <44289192+FionnMurphy@users.noreply.github.com>
2021-08-10 13:57:04 +0100

    removed scrolling while menu open

--- gwtf1/logic.js
+++ gwtf1/logic.js
@@ -1,4 +1,10 @@
+var menu = false;
 
-function exit_menu(){
-    document.getElementById('hamburger_menu').checked = false;
+function toggle_menu(){
+    if(menu){
+        document.getElementById('hamburger_menu').checked = false;
+        document.body.style.overflow = 'auto';
+    }else{
+        document.body.style.overflow = 'hidden';
+    }
 };
