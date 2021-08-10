72f23e00cf325e9eac9a1dc621f7f03ca935aab5
FionnMurphy <44289192+FionnMurphy@users.noreply.github.com>
2021-08-10 14:18:57 +0100

    menu

--- gwtf1/logic.js
+++ gwtf1/logic.js
@@ -2,9 +2,11 @@ var menu = false;
 
 function toggle_menu(){
     if(menu){
+        menu = false;
         document.getElementById('hamburger_menu').checked = false;
         document.body.style.overflow = 'auto';
     }else{
+        menu = true;
         document.body.style.overflow = 'hidden';
     }
 };
