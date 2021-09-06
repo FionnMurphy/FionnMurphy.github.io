c59052926d6d51d3c3f75e4c12f066692babb184
FionnMurphy <44289192+FionnMurphy@users.noreply.github.com>
2021-09-06 17:56:26 +0100

    nav for mobile

---
+++ logic.js
@@ -0,0 +1,8 @@
+function toggle_menu(){
+    var x = document.getElementById('nav_bar');
+    if (x.style.display == 'none'){
+      x.style.display = 'block';
+    }else{
+      x.style.display = 'none';
+    }
+};
