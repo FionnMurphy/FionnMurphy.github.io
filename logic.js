7f5bd76f40957fb65ae043a5ce71d66998a95dd7
FionnMurphy <44289192+FionnMurphy@users.noreply.github.com>
2021-09-07 14:35:56 +0100

    updated menu

--- logic.js
+++ logic.js
@@ -12,10 +12,12 @@ function checksize(){
 };
 
 function toggle_menu(){
+  if(window.innerWidth <= 1000){
     var x = document.getElementById('nav_bar');
     if (x.style.display == 'none'){
       x.style.display = 'block';
     }else{
       x.style.display = 'none';
     };
+  };
 };
