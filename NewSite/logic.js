e12b36f5ce5538edced390a9688c3e1477481005
fionn.murphy@ucdconnect.ie <fionnmurphy1@gmail.com>
2022-08-03 17:45:36 +0100

    added technologies area

--- NewSite/logic.js
+++ NewSite/logic.js
@@ -1,3 +1,12 @@
 function closeMenu(){
     document.getElementsByClassName("Checkbox")[0].checked = false
 }
+
+const navBar = document.getElementsByTagName("nav")[0]
+window.addEventListener("scroll", () => {
+    if(window.scrollY === 0 && window.innerWidth >= 1000){
+        navBar.style.backgroundColor = "#00000000"
+    }else{
+        navBar.style.backgroundColor = "#ffffff"
+    }
+})
